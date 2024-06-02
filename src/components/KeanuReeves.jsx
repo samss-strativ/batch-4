6a7404a4d09cae423b2384aec924a54c2e891a6f
samss-strativ <samss.jubair@strativ.se>
2024-06-02 17:12:09 +0600

    Change image width

--- src/components/KeanuReeves.jsx
+++ src/components/KeanuReeves.jsx
@@ -3,6 +3,7 @@ const KeanuReeves = () => {
     <div>
       <h2>Keanu Reeves</h2>
       <img
+      className="w-50"
         src="https://deadline.com/wp-content/uploads/2023/03/Keanu-Reeves-constantine-wolverine.jpg"
         alt="Keanu Reeves"
       />
